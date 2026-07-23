383c27daf73e4a88fb921099f5233cf20aa944cc
muhammadadmiral <admelar15@gmail.com>
2026-07-24 03:55:37 +0700

    feat: implement base script logic

--- main.cpp
+++ main.cpp
@@ -1,23 +1,34 @@
+#include "sdk/inc/main.h"
+#include "sdk/inc/natives.h"
 #include <windows.h>
-#include "main.h"
-#include "natives.h"
 
-void ScriptMain() {
-    WAIT(2000);
-
-    while (true) {
-        WAIT(0);
-    }
+void ShowNotification(const char *message) {
+  HUD::BEGIN_TEXT_COMMAND_THEFEED_POST("STRING");
+  HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(message);
+  HUD::END_TEXT_COMMAND_THEFEED_POST_TICKER(false, false);
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
-        scriptRegister(hModule, ScriptMain);
-        break;
-    case DLL_PROCESS_DETACH:
-        scriptUnregister(hModule);
-        break;
+void ScriptMain() {
+  scriptWait(2000);
+  ShowNotification("Manual Transmission Mod Loaded!");
+  while (true) {
+    scriptWait(0);
+    Ped playerPed = PLAYER::PLAYER_PED_ID();
+    if (PED::IS_PED_IN_ANY_VEHICLE(playerPed, FALSE)) {
+      Vehicle veh = PED::GET_VEHICLE_PED_IS_IN(playerPed, FALSE);
     }
-    return TRUE;
+  }
+}
+
+BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved) {
+  switch (reason) {
+  case DLL_PROCESS_ATTACH:
+    DisableThreadLibraryCalls(hInstance);
+    scriptRegister(hInstance, ScriptMain);
+    break;
+  case DLL_PROCESS_DETACH:
+    scriptUnregister(hInstance);
+    break;
+  }
+  return TRUE;
 }
