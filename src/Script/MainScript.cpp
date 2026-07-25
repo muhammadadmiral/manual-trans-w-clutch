597437061e3d4411716507a62cfd7d63a2d874e5
muhammadadmiral <admelar15@gmail.com>
2026-07-26 02:14:12 +0700

    feat: implement main script loop and vehicle calibration workflow for manual transmission system

--- src/Script/MainScript.cpp
+++ src/Script/MainScript.cpp
@@ -240,7 +240,7 @@ void ScriptMain() {
       const VehicleOffsets &off = VehicleData::GetResolvedOffsets();
       const std::string bv = VehicleData::GetGameBuildVersion();
       char notify[256]{};
-      sprintf_s(notify, "Manual trans r3: %s | build %s | G:%X N:%X RPM:%X CLT:%X",
+      sprintf_s(notify, "Manual trans r4: %s | build %s | G:%X N:%X RPM:%X CLT:%X",
                 VehicleData::GetOffsetSourceName(),
                 bv.empty() ? "?" : bv.c_str(), off.Gear, off.NextGear, off.RPM,
                 off.Clutch);
